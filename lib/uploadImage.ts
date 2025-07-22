7552b61eede2e52f61c6b621b436d341236a728e
bryanwongkc <bryanwongkc@gmail.com>
2025-07-22 23:51:06 +0800

    -

--- lib/uploadImage.ts
+++ lib/uploadImage.ts
@@ -1,12 +1,9 @@
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { v4 as uuidv4 } from "uuid";
-import { app } from "./firebase";
-
-const storage = getStorage(app);
+import { storage } from "./firebase"; // make sure firebase.ts exports `storage`
 
 export async function uploadImage(file: File): Promise<string> {
   const imageRef = ref(storage, `images/${uuidv4()}-${file.name}`);
   await uploadBytes(imageRef, file);
-  const url = await getDownloadURL(imageRef);
-  return url;
+  return getDownloadURL(imageRef);
 }
